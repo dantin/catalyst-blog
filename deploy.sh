6f73a43130618bbb7de5aa8524de51a73f0c8656
dantin <chengjie.ding@gmail.com>
2016-12-26 23:12:41 +0800

    clean directories except 'static'

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 BUNDLE_NAME="catalyst"
-DEPLOY_DIR="/var/www/$BUNDLE_NAME/"
+#DEPLOY_DIR="/var/www/$BUNDLE_NAME"
+DEPLOY_DIR="/usr/local/var/www/$BUNDLE_NAME"
 
 cd `dirname $0`
 cd ..
 cd $BUNDLE_NAME
 ROOT_DIR=`pwd`
 git pull
+rm -rf public
 hexo generate
 tar -czf ${BUNDLE_NAME}.tar.gz -C public .
+# remove old files except 'static' directory
+find $DEPLOY_DIR -not \( -type f -regex '.*/static/*.*' -prune \) -type f -print0 | xargs -0 rm -f
+# clear empty directory
+find $DEPLOY_DIR -type d -empty -print0 | xargs -0 sudo rmdir
 sudo tar -zxf ${BUNDLE_NAME}.tar.gz -C $DEPLOY_DIR
+# change file permissions
+# no need to grant to root:root
+# sudo chown -R root:root $DEPLOY_DIR
+find $DEPLOY_DIR -type d -print0 | xargs -0 sudo chmod 755
+find $DEPLOY_DIR -type f -print0 | xargs -0 sudo chmod 644
