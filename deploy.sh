0491be916424f6808199bfe1e558e88a7eddb74e
dantin <chengjie.ding@gmail.com>
2017-01-12 15:56:39 +0800

    change generate function from hexo to hugo

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,23 @@
 BUNDLE_NAME="catalyst"
 #DEPLOY_DIR="/var/www/$BUNDLE_NAME"
 DEPLOY_DIR="/usr/local/var/www/$BUNDLE_NAME"
+VENV_DIR="/home/david/Documents/venv/devops"
 
+source $VENV_DIR/bin/activate
 cd `dirname $0`
 cd ..
 cd $BUNDLE_NAME
 ROOT_DIR=`pwd`
 git pull
 rm -rf public
-hexo generate
+#hexo generate
+$GOPATH/bin/hugo --baseURL http://www.dding.info/
 tar -czf ${BUNDLE_NAME}.tar.gz -C public .
 # remove old files except 'static' directory
-find $DEPLOY_DIR -not \( -type f -regex '.*/static/*.*' -prune \) -type f -print0 | xargs -0 rm -f
+#find $DEPLOY_DIR -not \( -type f -regex '.*/static/*.*' -prune \) -type f -print0 | xargs -0 rm -f
 # clear empty directory
+#find $DEPLOY_DIR -type d -empty -print0 | xargs -0 sudo rmdir
+find $DEPLOY_DIR -type f -print0 | xargs -0 rm -f
 find $DEPLOY_DIR -type d -empty -print0 | xargs -0 sudo rmdir
 sudo tar -zxf ${BUNDLE_NAME}.tar.gz -C $DEPLOY_DIR
 # change file permissions
